56691b3101b567d8e179199238c5e6fe5b6dab97
JoanLonsan <joancres12@gmail.com>
2022-09-20 18:52:59 +0200

    Add files via upload

---
+++ libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcrescen <jcrescen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 16:21:21 by jcrescen          #+#    #+#             */
+/*   Updated: 2022/09/20 18:09:49 by jcrescen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# define LIBFT_H
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <ctype.h>
+# include <string.h>
+
+//listado de PROTOTYPES.
+
+#endif
