e2bed3f137a43867e89168ce2a375065243b947d
JoanLonsan <joancres12@gmail.com>
2022-09-27 14:14:45 +0200

    Day5 - Confirmation all uploaded

--- wip_ft_split.c
+++ wip_ft_split.c
@@ -12,12 +12,49 @@
 
 #include "libft.h"
 
-/*
-ft_strchr;
-ft_strlen;
-*/
+static char	ft_splitaux(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	char t;
+
+	i = 0;
+	while (s[i] != c || s[i])
+		i++;
+	t = (char *)malloc(sizeof(char) * i);
+	j = 0;
+	while (s[j] != c || s[j])
+	{
+		t[j] = s[j];
+		j++;
+	}
+	t[j] = '\0';
+	return (t);
+}
 
 char    **ft_split(char const *s, char c)
 {
-    
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	j = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] != c)
+			i++;
+		j++;
+	}
+	str = (char *)malloc(sizeof (char) * (j + 1));
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (s[j] != c)
+		{
+			str[i] = ft_splitaux(s[j], c);
+			free(*str);
+			i++;
+	}
 }
